9a8e0a694d80970742a0dfcff31b9e1ea8ae877f
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 17:08:15 -0300

    create layout page for the workspaceId route

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,10 @@
+import { Toolbar } from "./Toolbar";
+
+export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main className="h-full">
+            <Toolbar />
+            {children}
+        </main>
+    );
+}
