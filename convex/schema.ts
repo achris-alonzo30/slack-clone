7dfe65744ae269f821a680f9ad09a101dab3829b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 15:08:39 -0300

    create new schema table called workspaces

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,11 @@ import { defineSchema, defineTable } from "convex/server";
 const schema = defineSchema({
   ...authTables,
 
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joinCode: v.string(),
+  })
   
 });
  
