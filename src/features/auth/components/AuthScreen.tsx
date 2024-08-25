d9e1d3effb32c4c3f2fa997e71ae73528baa8a24
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 12:42:32 -0300

    create SignIn and SignUp card component for auth feature and render them dynamically in AuthScreen component

--- src/features/auth/components/AuthScreen.tsx
+++ src/features/auth/components/AuthScreen.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { AuthFlow } from "../types";
+import { SignInCard } from "./SignInCard";
+import { SignUpCard } from "./SignUpCard";
+
 export const AuthScreen = () => {
-  return <div>AuthScreen</div>;
+  const [authFlow, setAuthFlow] = useState<AuthFlow>("signIn"); 
+
+  return (
+    <section className="h-full flex items-center justify-center bg-[#5C3B58]">
+      <aside className="md:h-auto md:w-[420px]">
+        {authFlow === "signIn" ? <SignInCard /> : <SignUpCard />}
+      </aside>
+    </section>
+  );
 };
