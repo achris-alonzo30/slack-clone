11fbab730d735038bb704c44a2a9bb30a599e694
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 16:11:38 -0300

    create API POST route for creating new workspaces

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,8 +1,29 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
 
 export const get = query({
     args: {},
     handler: async (ctx) => {
         return await ctx.db.query("workspaces").collect();
+    }
+})
+
+export const create = mutation({
+    args: { name: v.string() },
+    handler: async (ctx, { name }) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (userId === null) {
+            throw new Error("Client is not authenticated!")
+        }
+
+        const joinCode = crypto.randomUUID();
+
+        return await ctx.db.insert("workspaces", {
+            name,
+            userId,
+            joinCode,
+        })
     }
 })
--- src/features/workspaces/components/CreateWorkspaceModal.tsx
+++ src/features/workspaces/components/CreateWorkspaceModal.tsx
@@ -5,6 +5,8 @@ import {
     DialogContent,
     DialogDescription,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { useModalState } from "../store/useModalState";
 
 export const CreateWorkspaceModal = () => {
@@ -13,6 +15,8 @@ export const CreateWorkspaceModal = () => {
     const handleClose = () => {
         setOpen(false);
     }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {}
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent className="sm:max-w-[425px]">
@@ -20,7 +24,29 @@ export const CreateWorkspaceModal = () => {
                     <DialogTitle>Create your workspace</DialogTitle>
                 </DialogHeader>
             </DialogContent>
-
+            <form className="space-y-4" onSubmit={handleSubmit}>
+                <fieldset>
+                    <label htmlFor="workspace-name">Workspace name</label>
+                    <Input 
+                        required
+                        autoFocus
+                        minLength={3}
+                        type="text" 
+                        id="workspace-name" 
+                        name="workspace-name"
+                        placeholder="Workspace name e.g 'Acme Corp'" 
+                        className="w-full"
+                    />
+                </fieldset>
+                <aside className="flex justify-end">
+                    <Button 
+                        type="submit"
+                        disabled={false}
+                    >
+                        Create
+                    </Button>
+                </aside>
+            </form>
         </Dialog>
     );
 };
