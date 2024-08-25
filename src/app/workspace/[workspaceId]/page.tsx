40be907dec46bb33462f68ef9ed3c89dfe04da77
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 17:04:13 -0300

    create custom hook for making API call on workspace table by getById route

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useGetWorkspaceById } from "@/features/workspaces/api/useGetWorkspaceById";
 import { useWorkspaceId } from "@/hooks/useWorkspaceId";
 
 export default function WorkspaceIdPage() {
     const workspaceId = useWorkspaceId();
+    const { workspace, isLoading } = useGetWorkspaceById({ workspaceId });
     return (
         <div>
             <h1>Workspace</h1>
---
+++ src/features/workspaces/api/useGetWorkspaceById.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+interface UseGetWorkspaceByIdProps {
+    workspaceId: Id<"workspaces">;
+}
+
+export const useGetWorkspaceById = ({ workspaceId }: UseGetWorkspaceByIdProps) => {
+    const workspace = useQuery(api.workspaces.getById, {workspaceId});
+    const isLoading = workspace === undefined;
+
+    return { workspace, isLoading };
+};
