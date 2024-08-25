a4de99a0f499cc55a4247abd54df428e37450cc6
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 12:55:35 -0300

    design SignInCard component

--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -1,3 +1,96 @@
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { 
+    Card, 
+    CardTitle,
+    CardHeader,
+    CardContent, 
+    CardDescription,   
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
 export const SignInCard = () => {
-  return (<div>SignInCard</div>);
+    return (
+        <Card className="w-full h-full p-8">
+            <CardHeader className="px-0 pt-0">
+                <CardTitle>
+                    Login to continue
+                </CardTitle>
+                <CardDescription>
+                    Use your email or another provider to login
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-5 px-0 pb-0">
+            <aside className="flex flex-col gap-y-2.5">
+                    <Button
+                        size="sm"
+                        disabled={false}
+                        onClick={() => { }}
+                        variant="outline"
+                        className="w-full relative"
+                    >
+                        <FcGoogle className="size-5 absolute top-3 left-2.5" />
+                        Continue with Google
+                    </Button>
+                    <Button
+                        size="sm"
+                        disabled={false}
+                        onClick={() => { }}
+                        variant="outline"
+                        className="w-full relative"
+                    >
+                        <FaGithub className="size-5 absolute top-3 left-2.5" />
+                        Continue with GitHub
+                    </Button>
+                </aside>
+
+                <Separator className="my-2" />
+
+                <form className="space-y-4">
+                    <fieldset>
+                        <label htmlFor="email" className="text-sm font-medium text-neutral-700">Email</label>
+                        <Input
+                            id="email"
+                            name="email"
+                            required
+                            type="email"
+                            value=""
+                            disabled={false}
+                            placeholder="Enter your email"
+                            onChange={() => { }}
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="password" className="text-sm font-medium text-neutral-700">Password</label>
+                        <Input
+                            id="password"
+                            name="password"
+                            required
+                            type="password"
+                            value=""
+                            disabled={false}
+                            placeholder="Enter your password"
+                            onChange={() => { }}
+                        />
+                    </fieldset>
+                    <Button
+                        size="sm"
+                        type="submit"
+                        disabled={false}
+                        className="w-full"
+                    >
+                        Continue with email
+                    </Button>
+                </form>
+                <p className="text-xs text-center text-neutral-500">
+                    Don&apos;t have an account? <Link href="/auth/sign-up" className="text-sky-700 hover:underline cursor-pointer">Sign up</Link>
+                </p>
+            </CardContent>
+        </Card>
+    );
 };
