088050764f3c03e48010396effa415f14e7ba3c1
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-25 21:20:28 -0300

    add Resizable component to the workspace layout aand create WorkspaceSidebar component

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -3,13 +3,40 @@
 import { Sidebar } from "./Sidebar";
 import { Toolbar } from "./Toolbar";
 
+import {
+    ResizablePanel,
+    ResizableHandle,
+    ResizablePanelGroup
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./WorkspaceSidebar";
+
 export default function WorkspaceLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="h-full">
             <Toolbar />
             <main className="flex h-[calc(100%-40px)]">
                 <Sidebar />
-                {children}
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    autoSaveId="workspace-layout"
+                >
+                    <ResizablePanel
+                        minSize={11}
+                        defaultSize={20}
+                        className="bg-[#5e2c5f]"
+                    >
+                        <WorkspaceSidebar />
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel
+                        minSize={20}
+                        defaultSize={80}
+                        className="bg-neutral-300"
+                    >
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+
             </main>
         </div>
     );
