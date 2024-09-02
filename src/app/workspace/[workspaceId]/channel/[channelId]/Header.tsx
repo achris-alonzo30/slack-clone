49f0bb5c9af089357b22eb3f1ec0f96bc725eaf2
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-02 19:56:27 -0300

    add frontend functionality for the Header component in channel sepage

--- src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/Header.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import { Trash } from "lucide-react";
 import { FaChevronDown } from "react-icons/fa";
 
 import {
@@ -6,13 +9,23 @@ import {
     DialogHeader,
     DialogContent,
     DialogTrigger,
+    DialogFooter,
+    DialogClose,
 } from "@/components/ui/dialog";
-
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Trash } from "lucide-react";
 
 
 export const Header = ({ channelName }: { channelName: string }) => {
+    const [editOpen, setEditOpen] = useState(false);
+    const [value, setValue] = useState(channelName);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+        setValue(value);
+    }
+
     return (
         <header className="bg-neutral-50 border-b h-[49px] flex items-center px-4 overflow-hidden">
             <Dialog>
@@ -32,13 +45,41 @@ export const Header = ({ channelName }: { channelName: string }) => {
                     <DialogTitle># {channelName}</DialogTitle>
                 </DialogHeader>
                 <div className="px-4 pb-4 flex flex-col gap-y-2">
-                    <div className="px-5 py-4 bg-neutral-50 rounded-lg border cursor-pointer hover:bg-neutral-100">
-                        <div className="flex items-center justify-between">
-                            <p className="text-sm font-semibold">Channel Name</p>
-                            <p className="text-sm text-[#126483] hover:underline font-semibold">Edit</p>
-                        </div>
-                        <p className="text-sm text-neutral-500 font-medium"># {channelName}</p>
-                    </div>
+                    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+                        <DialogTrigger asChild>
+                            <div className="px-5 py-4 bg-neutral-50 rounded-lg border cursor-pointer hover:bg-neutral-100">
+                                <div className="flex items-center justify-between">
+                                    <p className="text-sm font-semibold">Channel Name</p>
+                                    <p className="text-sm text-[#126483] hover:underline font-semibold">Edit</p>
+                                </div>
+                                <p className="text-sm text-neutral-500 font-medium"># {channelName}</p>
+                            </div>
+                        </DialogTrigger>
+                        <DialogContent className="">
+                            <DialogHeader className="p-4 border-b bg-white">
+                                <DialogTitle>Rename channel</DialogTitle>
+                            </DialogHeader>
+                            <form className="space-y-4" onSubmit={()=> {}}>
+                                <Input
+                                    required
+                                    autoFocus
+                                    minLength={3}
+                                    value={value}
+                                    maxLength={80}
+                                    disabled={false}
+                                    onChange={handleChange}
+                                    placeholder="Channel name"
+                                />
+                                <DialogFooter>
+                                    <DialogClose asChild>
+                                        <Button variant="outline" disabled={false}>Cancel</Button>
+                                    </DialogClose>
+                                    <Button disabled={false}>Save</Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+
                     <button className="flex items-center gap-x-2 px-5 py-4 bg-neutral-50 rounded-lg border cursor-pointer hover:bg-neutral-100 text-rose-500">
                         <Trash className="size-4" />
                         <p className="text-sm font-semibold">Delete channel</p>
