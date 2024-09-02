4fbc1b1390b31b75ea586db1b419591c0eee9089
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-02 15:48:47 -0300

    create GETALL function for the members table

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,11 @@
 import { v } from "convex/values"
-import { query } from "./_generated/server"
+import { Id } from "./_generated/dataModel"
+import { query, QueryCtx } from "./_generated/server"
 import { getAuthUserId } from "@convex-dev/auth/server"
+
+const populateMember = (ctx: QueryCtx, userId: Id<"users">) => {
+    return ctx.db.get(userId)
+}
 
 
 export const get = query({
@@ -18,5 +23,41 @@ export const get = query({
         if (!member) return null;
 
         return member;
+    }
+})
+
+export const getAll = query({
+    args: { workspaceId: v.id("workspaces") },
+    handler: async (ctx, { workspaceId }) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) return [];
+
+        const member = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_and_user_id", q => q.eq("workspaceId", workspaceId).eq("userId", userId))
+            .unique();
+
+        if (!member) return [];
+
+        const data = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id", q => q.eq("workspaceId", workspaceId))
+            .collect();
+
+        const members = [];
+
+        for (const member of data) {
+            const user = await populateMember(ctx, member.userId);
+
+            if (user) {
+                members.push({
+                    ...member,
+                    user
+                })
+            }
+        }
+
+        return members;
     }
 })
