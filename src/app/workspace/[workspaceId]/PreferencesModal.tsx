e5825066021c479925f1d9158371ec5ca9981d80
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-02 13:50:09 -0300

    design the content for the PrederencesModal component

--- src/app/workspace/[workspaceId]/PreferencesModal.tsx
+++ src/app/workspace/[workspaceId]/PreferencesModal.tsx
@@ -8,6 +8,7 @@ import {
     DialogDescription,
 } from "@/components/ui/dialog";
 import { useState } from "react";
+import { Trash } from "lucide-react";
 
 interface PreferencesModalProps {
     isOpen: boolean;
@@ -24,13 +25,27 @@ export const PreferencesModal = ({
     const [value, setValue] = useState(intialValue);
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Preferences</DialogTitle>
-                    <DialogDescription>
-                        Customize your workspace preferences
-                    </DialogDescription>
+            <DialogContent className="p-0 bg-neutral-50 overflow-hidden">
+                <DialogHeader className="p-4 border-b bg-white">
+                    <DialogTitle>{value}</DialogTitle>
                 </DialogHeader>
+                <div className="px-4 pb-4 flex flex-col gap-y-2">
+                    <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-neutral-50">
+                        <div className="flex ittems-center justify-between">
+                            <p className="text-sm font-semibold">Workspace Name</p>
+                            <p className="text-sm text-[#1264a3] hover:underline font-semibold">Edit</p>
+                        </div>
+                        <p className="text-sm">{value}</p>
+                    </div>
+                    <button 
+                        disabled={false}
+                        onClick={() => {}}
+                        className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-neutral-50 text-rose-500"
+                    >
+                        <Trash className="size-4"/>
+                        <p className="text-sm font-semibold">Delete workspace</p>
+                    </button>
+                </div>
             </DialogContent>
         </Dialog>
     )
