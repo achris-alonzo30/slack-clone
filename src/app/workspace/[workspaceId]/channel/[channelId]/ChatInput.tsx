e42af47922e6a17501a92a05da048c6ce416b4fb
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-10 16:35:01 -0300

    implement the custom create message mutation in the ChatInput component

--- src/app/workspace/[workspaceId]/channel/[channelId]/ChatInput.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/ChatInput.tsx
@@ -1,6 +1,9 @@
 import Quill from "quill";
 import { useRef } from "react";
 import dynamic from "next/dynamic";
+import { useCreateMessage } from "@/features/messages/api/useCreateMessage";
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { useChannelId } from "@/hooks/useChannelId";
 
 
 const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
@@ -11,6 +14,10 @@ export const ChatInput = ({
     placeholder: string;
 }) => {
     const innerRef = useRef<Quill | null>(null);
+    const { mutate } = useCreateMessage();
+
+    const workspaceId = useWorkspaceId();
+    const channelId = useChannelId();
 
     const handleSubmit = ({
         body,
@@ -19,9 +26,13 @@ export const ChatInput = ({
         body: string;   
         image: File | null;
     }) => {
-        
+        mutate({
+            body,
+            channelId,
+            workspaceId
+        })
     }
-
+    
     return (
         <div className="px-5 w-full">
             <Editor 
