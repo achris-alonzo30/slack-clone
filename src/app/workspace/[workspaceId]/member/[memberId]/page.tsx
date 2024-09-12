0f8d644bb84b47e23f05ddac975f7d97e8821378
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-12 17:52:13 -0300

    create new api endpoint for conversations in convex

--- src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { useMemberId } from "@/hooks/useMemberId";
 import { useWorkspaceId } from "@/hooks/useWorkspaceId";
 
 export default function MemberPage() {
+    const memberId = useMemberId();
     const workspaceId = useWorkspaceId();
-
-
     
     return (
         <></>
