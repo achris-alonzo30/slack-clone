f96ce33c2aa96cd5f96dc1bc181faafed1508370
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-14 20:16:06 -0300

    add the useProfileMemberId from store to the custom hook usePanel

--- src/hooks/usePanel.ts
+++ src/hooks/usePanel.ts
@@ -1,19 +1,30 @@
+import { useProfileMemberId } from "@/features/members/store/useProfileMemberId";
 import { useParentMessageId } from "@/features/messages/store/useParentMessageId";
 
 export const usePanel = () => {
     const [parentMessageId, setParentMessageId] = useParentMessageId();
+    const [profileMemberId, setProfileMemberId] = useProfileMemberId();
+
+    const onOpenProfile = (memberId: string) => {
+        setProfileMemberId(memberId);
+        setParentMessageId(null);
+    }
 
     const onOpenMessage = (messageId: string) => {
         setParentMessageId(messageId);
+        setProfileMemberId(null);
     }
 
     const onClose = () => {
         setParentMessageId(null);
+        setProfileMemberId(null);
     };
 
     return {
         onClose,
         onOpenMessage,
+        onOpenProfile,
         parentMessageId,
+        profileMemberId,
     }
 }
