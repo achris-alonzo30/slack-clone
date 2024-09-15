53a3a492b3b8713a4c653e57f4c5fe213db89b75
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-14 22:25:05 -0300

    update convex channels and worspaces api for their remove function to delete other relationships

--- convex/channels.ts
+++ convex/channels.ts
@@ -132,7 +132,16 @@ export const remove = mutation({
         
         if (!member || member.role !== "admin") throw new Error("Unauthorized");
 
-        // TODO: Delete all messages in the channel
+        const [messages] = await Promise.all([
+            ctx.db
+                .query("messages")
+                .withIndex("by_channel_id", (q) => q.eq("channelId", channelId))
+                .collect(),
+        ]);
+
+        for (const message of messages) {
+            await ctx.db.delete(message._id);
+        };
 
         await ctx.db.delete(channelId);
 
