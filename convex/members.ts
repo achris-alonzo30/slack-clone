97ec6c77335f1a35c3465ef8da28f9646f575e84
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-09-14 21:00:45 -0300

    create update and remove function for convex members api

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values"
 import { Id } from "./_generated/dataModel"
-import { query, QueryCtx } from "./_generated/server"
+import { mutation, query, QueryCtx } from "./_generated/server"
 import { getAuthUserId } from "@convex-dev/auth/server"
 
 const populateMember = (ctx: QueryCtx, userId: Id<"users">) => {
@@ -89,4 +89,110 @@ export const getAll = query({
 
         return members;
     }
-})
+});
+
+export const update = mutation({
+    args: { 
+        memberId: v.id("members"), 
+        role: v.union(v.literal("admin"), v.literal("member"))
+    },
+    handler: async (ctx, { memberId, role }) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const member = await ctx.db.get(memberId);
+
+        if (!member) {
+            throw new Error("Member not found");
+        }
+
+        const currentMember = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_and_user_id", q => q.eq("workspaceId", member.workspaceId).eq("userId", userId))
+            .unique();
+
+        if (!currentMember || currentMember.role !== "admin") {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.patch(memberId, { role });
+
+        return memberId;
+    }
+});
+
+export const remove = mutation({
+    args: { 
+        memberId: v.id("members"), 
+    },
+    handler: async (ctx, { memberId }) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const member = await ctx.db.get(memberId);
+
+        if (!member) {
+            throw new Error("Member not found");
+        }
+
+        const currentMember = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_and_user_id", q => q.eq("workspaceId", member.workspaceId).eq("userId", userId))
+            .unique();
+
+        if (!currentMember) {
+            throw new Error("Unauthorized");
+        }
+
+        if (member.role === "admin") {
+            throw new Error("Cannot remove admin");
+        }
+
+        const isSelf = currentMember._id === memberId;
+        const isSelfAdmin = currentMember.role === "admin";
+        
+        if (isSelf && isSelfAdmin) {
+            throw new Error("Cannot remove self");
+        }
+
+        const [ messages, reactions, conversations ] = await Promise.all([
+            ctx.db
+                .query("messages")
+                .withIndex("by_member_id", q => q.eq("memberId", memberId))
+                .collect(),
+            ctx.db
+                .query("reactions")
+                .withIndex("by_member_id", q => q.eq("memberId", memberId))
+                .collect(),
+            ctx.db
+                .query("conversations")
+                .filter(q => q.or(
+                        q.eq(q.field("memberOneId"), member._id),
+                        q.eq(q.field("memberTwoId"), member._id),
+                    ))
+                .collect()
+        ]);
+
+        for (const message of messages) {
+            await ctx.db.delete(message._id);
+        }
+
+        for (const reaction of reactions) {
+            await ctx.db.delete(reaction._id);
+        }
+
+        for (const conversation of conversations) {
+            await ctx.db.delete(conversation._id);
+        }
+
+        await ctx.db.delete(memberId);
+
+        return memberId;
+    }
+});
--- src/features/members/components/Profile.tsx
+++ src/features/members/components/Profile.tsx
@@ -1,13 +1,103 @@
+import { Button } from "@/components/ui/button";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useGetMemberById } from "../api/useGetMemberById";
+
+import { AlertTriangle, Loader2, MailIcon, X } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export const Profile = ({
     onClose,
     memberId,
-} : {
+}: {
     onClose: () => void;
     memberId: Id<"members">;
 }) => {
+    const { member, isLoading } = useGetMemberById({ memberId });
+    if (isLoading) {
+        return (
+            <div className="h-full flex flex-col">
+                <div className="flex justify-between items-center h-[49px] px-4 border-b">
+                    <p className="text-lg font-bold">Profile</p>
+                    <Button
+                        size="iconSm"
+                        variant="ghost"
+                        onClick={onClose}
+                    >
+                        <X className="size-5 stroke-[1.5" />
+                        <span className="sr-only">Close Profile Panel</span>
+                    </Button>
+                </div>
+                <div className="h-full flex items-center justify-center">
+                    <Loader2 className="size-8 animate-spin text-muted-foreground" />
+                </div>
+            </div>
+        )
+    };
+
+    if (!member) {
+        return (
+            <div className="h-full flex flex-col">
+                <div className="flex justify-between items-center h-[49px] px-4 border-b">
+                    <p className="text-lg font-bold">Profile</p>
+                    <Button
+                        size="iconSm"
+                        variant="ghost"
+                        onClick={onClose}
+                    >
+                        <X className="size-5 stroke-[1.5" />
+                        <span className="sr-only">Close Profile Panel</span>
+                    </Button>
+                </div>
+                <div className="h-full flex flex-col gap-y-2 items-center justify-center">
+                    <AlertTriangle className="size-8 text-muted-foreground" />
+                    <p className="text-sm text-muted-foreground">Profile not found</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <></>
+        <section className="h-full flex flex-col">
+            <aside className="flex justify-between items-center h-[49px] px-4 border-b">
+                <p className="text-lg font-bold">Profile</p>
+                <Button
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={onClose}
+                >
+                    <X className="size-5 stroke-[1.5" />
+                    <span className="sr-only">Close Profile Panel</span>
+                </Button>
+            </aside>
+            <aside className="flex flex-col items-center justify-center p-4">
+                <Avatar className="max-w-[256px] max-h-[256px] size-5 rounded-md mr-1">
+                    <AvatarImage className="object-cover rounded-md" src={member.user.image} />
+                    <AvatarFallback className="rounded-md text-6xl font-medium aspect-square text-neutral-300 bg-neutral-950">{member.user.name?.charAt(0).toUpperCase()}</AvatarFallback>
+                </Avatar>
+            </aside>
+            <aside className="flex flex-col p-4">
+                <p className="text-xl font-bold">{member.user.name}</p>
+            </aside>
+            <Separator />
+            <aside className="flex flex-col p-4">
+                <p className="text-sm font-bold mb-4">Contact Information:</p>
+                <div className="flex items-center gap-2">
+                    <div className="size-9 rounded-md bg-muted flex items-center justify-center">
+                        <MailIcon className="size-4" />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-[13px] font-semibold text-muted-foreground">Email Address</p>
+                    </div>
+                    <Link
+                        href={`mailto:${member.user.email}`}
+                        className="text-sm hover:underline text-[#1264a3]"
+                    >
+                        {member.user.email}
+                    </Link>
+                </div>
+            </aside>
+        </section>
     )
 }
